ec190b5bded92b8f02c773a99a375468ce44c2d2
Alexandre Lopes <alexandre.xplopes@gmail.com>
2026-06-28 16:01:52 -0300

    feat: adiciona variaveis de ambiente para proteger as credenciais do banco

--- repository/bd.js
+++ repository/bd.js
@@ -1,12 +1,12 @@
 const { Pool } = require('pg');
+require('dotenv').config(); // biblioteca para ler o arquivo .env
 
-// O ideal no futuro é trocar isso por variáveis de ambiente (.env)
 const pool = new Pool({
-    user: "postgres",
-    password: "sua_senha_aqui",
-    host: "localhost",
-    port: 5432,
-    database: "biblioteca_db"
+    user: process.env.DB_USER || "postgres",
+    password: process.env.DB_PASSWORD, // puxa a senha do arquivo .env
+    host: process.env.DB_HOST || "localhost",
+    port: process.env.DB_PORT || 5432,
+    database: process.env.DB_NAME || "biblioteca_db"
 });
 
 async function connect() {
